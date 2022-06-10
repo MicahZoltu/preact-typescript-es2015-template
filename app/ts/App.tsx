692a6131e5ee81fdfca62e71f867dd979597a2f9
Micah Zoltu <micah@zoltu.net>
2022-06-10 13:41:01 +0800

    Modernizes the template.
    Also includes the proxy from react template, and adds some interactivity to showcase that this is actually a functioning SPA and not just a static page.

--- app/ts/App.tsx
+++ app/ts/App.tsx
@@ -1,7 +1,12 @@
-interface AppModel {
+export interface AppModel {
+	readonly cycleGreeting: () => void
 	greeting: string
+	subject: string
 }
 
-export function App(model: AppModel) {
-	return <div>{model.greeting} World!</div>
+export function App(model: Readonly<AppModel>) {
+	return <>
+		<div>{model.greeting} {model.subject}</div>
+		<button onClick={model.cycleGreeting}>Change</button>
+	</>
 }
--- app/ts/index.ts
+++ app/ts/index.ts
@@ -1,4 +1,23 @@
 import { createElement, render } from 'preact'
-import { App } from './App'
+import { App, AppModel } from './App'
+import { createOnChangeProxy } from './proxy'
 
-render(createElement(App, { greeting: 'Hello' }), document.body)
+// create our root model as a proxy object that will auto-rerender anytime its properties (recursively) change
+const rootModel: AppModel = createOnChangeProxy<AppModel>(rerender, {
+	cycleGreeting: () => rootModel.greeting = (rootModel.greeting === 'Hello') ? 'nuqneH' : 'Hello',
+	greeting: 'Hello',
+	subject: 'World',
+})
+
+// put the root model on the window for debugging convenience
+declare global { interface Window { rootModel: AppModel } }
+window.rootModel = rootModel
+
+// specify our render function, which will be fired anytime rootModel is mutated
+function rerender() {
+	const element = createElement(App, rootModel)
+	render(element, document.body)
+}
+
+// kick off the initial render
+rerender()
